d4eb21c68082face7d6cd1b75ce2bd85e5fbe55f
Gabriel Santos <gabrielrsantoss@icloud.com>
2025-10-31 21:43:21 -0300

    add: post favorites endpoint

--- moonet/resources/favorites.py
+++ moonet/resources/favorites.py
@@ -5,11 +5,22 @@ from flask_restful import Api, Resource
 
 from ..store import FavoritesStore
 from ..utils.response import build_response
+from ..utils.ytdlp import yt_video_info
 
 store = FavoritesStore()
 
 
 class FavoritesResource(Resource):
+
+  def post(self):
+    data = request.get_json() or {}
+    url = data.get("url")
+    if not url:
+      return build_response(False, "URL is required"), 400
+    video = yt_video_info(url)
+    store.add(video)
+    return build_response(True), 201
+
   def get(self):
     args = request.args or {}
     limit = int(args.get("limit", 50))
